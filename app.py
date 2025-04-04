4d81a46de0cba01da991b2ab6f0f1447126f6926
pheinxzhzh <braddockrivelrhc8@gmail.com>
2025-04-04 21:11:55 +0800

    随机数增加

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-print ("hello")
+
+@app.route("/data")
+def get_data():
+    # 一次返回多个随机数
+    return jsonify({
+        'a': random.randint(1, 100),
+        'b': random.randint(101, 200),
+        'c': random.randint(201, 300)
+    })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # 读取 Render 指定的端口
-    app.run(host="0.0.0.0", port=port)        # 绑定到所有 IP（对外开放）
+    app.run(host="0.0.0.0", port=5000)
