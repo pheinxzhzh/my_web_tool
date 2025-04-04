6858614920fab4947b685db09f5ca2e855f64e3f
pheinxzhzh <braddockrivelrhc8@gmail.com>
2025-04-04 16:06:15 +0800

    更新 app

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+print ("hello")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # 读取 Render 指定的端口
