13dc9d0168d59d97be74038e29625f56b25d0a1b
pheinxzhzh <braddockrivelrhc8@gmail.com>
2025-04-04 15:15:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,8 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))  # 读取 Render 指定的端口
+    app.run(host="0.0.0.0", port=port)        # 绑定到所有 IP（对外开放）
